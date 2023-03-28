8e4341df8b7ff87ab12926e4cb9dd5a25275624d
Catherine He <hecy@usc.edu>
2023-03-28 16:13:51 +0000

    Initial commit for hw5

---
+++ wordle.cpp
@@ -0,0 +1,78 @@
+#ifndef RECCHECK
+// For debugging
+#include <iostream>
+// For std::remove
+#include <algorithm> 
+#include <map>
+#include <set>
+#endif
+
+#include "wordle.h"
+#include "dict-eng.h"
+using namespace std;
+
+
+// Add prototypes of helper functions here
+
+
+// Definition of primary wordle function
+std::set<std::string> wordle(
+    const std::string& in,
+    const std::string& floating,
+    const std::set<std::string>& dict)
+{
+    std::set<std::string> words;
+
+    if (floating.size() == 0)
+    {
+        bool allFixed = true;
+        for (int i = 0; i < in.size(); i++)
+        {
+            if (in[i] == '-')
+            {
+                char alpha = 'a';
+                for (int a = 0; a < 26; a++)
+                {
+                    std::string temp = in;
+                    temp[i] = alpha;
+                    std::set<std::string> newSet = wordle(temp, floating, dict);
+                    words.insert(newSet.begin(), newSet.end());
+                    alpha++;
+                }
+                allFixed = false;
+            }
+        }
+        if (allFixed)
+        {
+            if (dict.find(n) != dict.end()){
+                words.insert(in);
+            }
+            return words;
+        }
+    }
+    else
+    {
+        char toFix = floating[0];
+        for (int d = 0; d < in.size(); d++)
+        {
+            std::string temp = in; 
+            if (in[d] == '-')
+            {
+                temp[d] = toFix;
+                std::set<std::string> newSet = wordle(temp, floating.substr(1), dict);
+                words.insert(newSet.begin(), newSet.end());
+            }
+        }
+        return words;
+    }
+    // try each floating letter at each spot
+    // if floating.size() == 0, randomly generate at each spot
+
+    // LENGTH of word:in.size()
+    
+
+    // Add your code here
+
+}
+
+// Define any helper functions here
